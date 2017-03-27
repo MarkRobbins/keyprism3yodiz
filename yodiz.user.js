70570014e486342b30793d86c0866afdefe26e37
Mark Robbins <mark.robbins@mrobbinsassoc.com>
2017-03-26 18:25:27 -0700

    Empty UserScript

---
+++ yodiz.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Keyprism3 Yodiz
+// @namespace    http://mrobbinsassoc.com/
+// @version      0.0.1
+// @description  Allows interaction of Yodiz, TheBrain and Visual Paradigm
+// @author       Mark Robbins
+// @match        https://app.yodiz.com/plan/pages/board.vz?cid=30887
+// @grant        none
+// @copyright    2017, Mark Robbins
+// @homepageURL  http://mrobbinsassoc.com/projects/keyprism3/vp/publish/
+// @supportURL   mailto:mark.robbins@mrobbinsassoc.com
+// @updateURL    https://openuserjs.org/meta/MarkRobbins/Keyprism3_Yodiz.meta.js
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// ==/UserScript==
+// ==OpenUserJS==
+// @author MarkRobbins
+// ==/OpenUserJS==
+
+console.log('Keyprism3 Yodiz');
